c6c1650be7af6e6bd581cdf61fa661538f3afaec
CJskii <czareczarek3@gmail.com>
2023-01-01 20:49:07 +0000

    initial on load screen

--- src/DOM/components.js
+++ src/DOM/components.js
@@ -2,9 +2,10 @@ class Components {
   constructor(body) {
     this.body = body;
     this.background = this._background(body);
-    this.header = this._header(this.background);
-    this.button = this._button(this.background);
-    this.boards = this._boards(this.background);
+    this.header = this._header();
+    //this.boards = this._boards();
+    this.form = this._nameForm();
+    this.footer = this._footer();
   }
 
   _background(body) {
@@ -15,28 +16,61 @@ class Components {
     return background;
   }
 
-  _header(background) {
+  _header(background = this.background) {
     // Create the title element
     const header = document.createElement("h1");
-    header.textContent = "Battleship Game";
+    header.textContent = "Battleship";
     header.classList.add("game-title");
     background.append(header);
     return header;
   }
 
-  _button(background) {
-    // Create the button element
-    const btn = document.createElement("button");
-    btn.classList.add("btn");
-    btn.textContent = "New Game";
-    background.append(btn);
+  _boards(background = this.background) {
+    // Create boards wrapper element
+    const boards = document.createElement("div");
+    boards.classList.add("boards-container");
+    background.append(boards);
+    return boards;
   }
 
-  _boards(background) {
-    // Create boards wrapper element
-    const container = document.createElement("div");
-    container.classList.add("boards-container");
-    background.append(container);
+  _nameForm(background = this.background) {
+    // Create form + label + input + submit elements
+    const form = document.createElement("form");
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    const submit = document.createElement("button");
+    input.id = "name";
+    input.placeholder = "Captain name...";
+    label.setAttribute("for", input);
+    label.textContent = "Enter player name:";
+    submit.textContent = "Submit";
+    submit.classList.add("btn");
+    submit.addEventListener("click", (e) => this.#submitBtn(e, input));
+    form.append(label);
+    form.append(input);
+    form.append(submit);
+    background.append(form);
+    return form;
+  }
+
+  _footer(background = this.background) {
+    const footer = document.createElement("div");
+    const paragraph = document.createElement("p");
+    const anchor = document.createElement("a");
+    footer.classList.add("footer");
+    paragraph.textContent = "Created by ";
+    anchor.textContent = "CJski";
+    anchor.target = "_blank";
+    anchor.href = "https://github.com/CJskii/Battleship-game";
+    paragraph.append(anchor);
+    footer.append(paragraph);
+    background.append(footer);
+    return footer;
+  }
+
+  #submitBtn(e, input, form = this.form) {
+    console.log(input.value);
+    form.remove();
   }
 }
 
